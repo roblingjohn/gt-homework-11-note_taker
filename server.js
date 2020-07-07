2f19b5a0e1d0d38dbe79ceff9b15fc4c1d8944ff
John Robling <roblingjohn@gmail.com>
2020-07-07 18:09:53 -0400

    fixed post route

--- server.js
+++ server.js
@@ -16,17 +16,24 @@ app.use(express.json());
 app.use(express.static("public"))
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
   });
 
-  app.get("/notes", function(req, res) {
+app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
   });
 
-  app.get("/api/notes", function(req, res) {
+app.get("/api/notes", function(req, res) {
     return res.json(notes);
   });
 
+app.post("/api/notes", function(req, res) {
+    let newNote = req.body;
+    fs.writeFile("db.json", newNote, function(err, res){
+      if(err) throw err;
+    })
+    res.json(newNote);
+})
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
