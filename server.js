5edf817b1ffb5e8e567315aac592dcee0d2ee154
John Robling <roblingjohn@gmail.com>
2020-07-06 21:47:46 -0400

    set up express.static

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
-var express = require("express");
-var path = require("path");
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const notes = fs.readFile("./db/db.json", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+    return data;
+})
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"))
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
   });
 
   app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
   });
 
-
+  app.get("/api/notes", function(req, res) {
+    return res.json(notes);
+  });
 
 
 app.listen(PORT, function() {
